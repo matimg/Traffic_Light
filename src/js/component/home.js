1fd035028ec0cb84c7958b645b8f36fd200dd6fe
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-28 20:04:54 +0000

    Se dibuja un shadow blanco

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,29 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("red");
 	return (
 		<div className="text-center mt-5">
-			<div
-				className="container bg-dark text-dark rounded"
-				style={{ width: "220px", height: "550px" }}>
+			<div className="container bg-dark text-dark rounded semaforo">
 				semaforo
 				<div
-					className="bg-danger rounded-circle border-danger text-danger m-3"
-					style={{ height: "150px" }}>
+					className="rounded-circle text-danger m-3 bg-danger foco luz"
+					onClick={() => setColor(console.log("hola"))}>
 					div1
 				</div>
-				<div
-					className="bg-warning rounded-circle text-warning m-3"
-					style={{ height: "150px" }}>
+				<div className="bg-warning rounded-circle text-warning m-3 foco">
 					div2
 				</div>
-				<div
-					className="bg-success rounded-circle text-success m-3"
-					style={{ height: "150px" }}>
+				<div className="bg-success rounded-circle text-success m-3 foco">
 					div3
 				</div>
 			</div>
