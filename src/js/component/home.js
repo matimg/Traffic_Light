a689a1712a740cbc1222bca92f0166917817864c
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-28 19:17:17 +0000

    Semaforo dibujado

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,18 +7,26 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div
+				className="container bg-dark text-dark rounded"
+				style={{ width: "220px", height: "550px" }}>
+				semaforo
+				<div
+					className="bg-danger rounded-circle border-danger text-danger m-3"
+					style={{ height: "150px" }}>
+					div1
+				</div>
+				<div
+					className="bg-warning rounded-circle text-warning m-3"
+					style={{ height: "150px" }}>
+					div2
+				</div>
+				<div
+					className="bg-success rounded-circle text-success m-3"
+					style={{ height: "150px" }}>
+					div3
+				</div>
+			</div>
 		</div>
 	);
 }
